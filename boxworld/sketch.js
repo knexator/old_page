e247aeebdc190a980ec5ac4b6885007739c341b2
knexator <daniel.hurtadoramirez314@gmail.com>
2019-11-20 13:37:51 +0100

    started boxworld

--- boxworld/sketch.js
+++ boxworld/sketch.js
@@ -1,3 +1,21 @@
+let ballImg;
+let playerImg;
+let playerballImg;
+let crateImg;
+let crateballImg;
+let floorImg;
+let wallImg;
+
+function preload() {
+  ballImg = loadImage('assets/ball.png');
+  playerImg = loadImage('assets/player.png');
+  playerballImg = loadImage('assets/playerball.png');
+  crateImg = loadImage('assets/crate.png');
+  crateballImg = loadImage('assets/crateball.png');
+  floorImg = loadImage('assets/floor.png');
+  wallImg = loadImage('assets/wall.png');
+}
+
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.style('display', 'block');  
@@ -5,6 +23,7 @@ function setup() {
 
 function draw() {
   background(32);
+  image(ballImg, 0, 0);
 }
 
 function windowResized() {
