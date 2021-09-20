945d6ad8a56cca8b863d8a51935cc11e5e15f8f8
knexator <daniel.hurtadoramirez314@gmail.com>
2021-09-20 21:08:11 +0200

    added wobbly stuff

--- undoTres/undotres_v3.js
+++ undoTres/undotres_v3.js
@@ -8,6 +8,142 @@ pintar.clearColor = PintarJS.Color.fromHex('F7A36B');
 let canvas = document.getElementById('canvas')
 //let ctx = canvas.getContext('webgl2')
 
+let globalT = 0.0;
+let last_t = null;
+
+class WobblyShader extends PintarJS.DefaultShader
+  {
+    get fragmentShaderTextureCode()
+    {
+        return `
+
+        vec2 coor = vec2(gl_FragCoord.x * 0.001, gl_FragCoord.y * 0.001) + (u_color.xy);
+        //gl_FragColor = vec4(, u_deltaTime, 1.0) + 0.0 * (clamp(texture2D(u_image, v_texCoord) + u_colorBooster, 0.0, 1.0) * u_color);
+
+        //if (0.5 < cnoise(vec3((vec2(gl_FragCoord.x / _ScreenParams.x, gl_FragCoord.y / _ScreenParams.y))*20.0, u_deltaTime*0.001))) {
+        if (0.1 < cnoise(vec3(coor * 30.0, u_deltaTime*0.001))) {
+            gl_FragColor = (clamp(texture2D(u_image, v_texCoord) + u_colorBooster, 0.0, 1.0) * u_color);
+        } else {
+            gl_FragColor = vec4(0.0);
+        }
+            //gl_FragColor = clamp(texture2D(u_image, v_texCoord) + u_colorBooster, 0.0, 1.0) * u_color * sin(u_deltaTime);
+            //gl_FragColor.a = min(1.0, max(0.0, cnoise(vec3(v_texCoord*8.0, u_deltaTime*0.1))));
+            //gl_FragColor.rgb *= min(gl_FragColor.a, 1.0);
+        `;
+    }
+
+    get uniformNames() {
+      return super.uniformNames.concat(["u_deltaTime"])
+    }
+
+    get fragmentShaderAdditionalUniforms() {
+     return `uniform float u_deltaTime;
+     vec3 mod289(vec3 x)
+      {
+      return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+
+      vec4 mod289(vec4 x)
+      {
+      return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+
+      vec4 permute(vec4 x)
+      {
+      return mod289(((x*34.0)+10.0)*x);
+      }
+
+      vec4 taylorInvSqrt(vec4 r)
+      {
+      return 1.79284291400159 - 0.85373472095314 * r;
+      }
+
+      vec3 fade(vec3 t) {
+      return t*t*t*(t*(t*6.0-15.0)+10.0);
+      }
+
+      // Classic Perlin noise
+      float cnoise(vec3 P)
+      {
+      vec3 Pi0 = floor(P); // Integer part for indexing
+      vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+      Pi0 = mod289(Pi0);
+      Pi1 = mod289(Pi1);
+      vec3 Pf0 = fract(P); // Fractional part for interpolation
+      vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+      vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+      vec4 iy = vec4(Pi0.yy, Pi1.yy);
+      vec4 iz0 = Pi0.zzzz;
+      vec4 iz1 = Pi1.zzzz;
+
+      vec4 ixy = permute(permute(ix) + iy);
+      vec4 ixy0 = permute(ixy + iz0);
+      vec4 ixy1 = permute(ixy + iz1);
+
+      vec4 gx0 = ixy0 * (1.0 / 7.0);
+      vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+      gx0 = fract(gx0);
+      vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+      vec4 sz0 = step(gz0, vec4(0.0));
+      gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+      gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+      vec4 gx1 = ixy1 * (1.0 / 7.0);
+      vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+      gx1 = fract(gx1);
+      vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+      vec4 sz1 = step(gz1, vec4(0.0));
+      gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+      gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+      vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+      vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+      vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+      vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+      vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+      vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+      vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+      vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+      vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+      g000 *= norm0.x;
+      g010 *= norm0.y;
+      g100 *= norm0.z;
+      g110 *= norm0.w;
+      vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+      g001 *= norm1.x;
+      g011 *= norm1.y;
+      g101 *= norm1.z;
+      g111 *= norm1.w;
+
+      float n000 = dot(g000, Pf0);
+      float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+      float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+      float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+      float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+      float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+      float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+      float n111 = dot(g111, Pf1);
+
+      vec3 fade_xyz = fade(Pf0);
+      vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+      vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+      float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+      return 2.2 * n_xyz;
+      }
+     `;
+   }
+
+    prepare(renderable, viewport)
+    {
+      super.prepare(renderable, viewport);
+      this.setUniformf(this.uniforms.u_deltaTime, globalT);
+      //deltaTime += 0.1;
+    }
+  }
+
+let wobblyShader = new WobblyShader();
+
 let TILE = 40
 let OFFX = TILE
 let OFFY = TILE
@@ -229,6 +365,7 @@ function drawSpr(spr, i,j) {
 }
 
 function drawLevel (level) {
+  pintar._renderer.setShader(null);
   if (in_last_level) {
     if (level.extraDrawCode) level.extraDrawCode()
     return
@@ -369,7 +506,7 @@ function drawLevel (level) {
     let crate_forward = get_times_directions(crate.history.length - 2)[inmune] == 1
     let ci = lerp(prevState[0], state[0], crate_forward ? forwardsT : backwardsT)
     let cj = lerp(prevState[1], state[1], crate_forward ? forwardsT : backwardsT)
-    drawSpr(crateSprs[inmune], ci, cj)
+    drawSpr(crate_sprites[inmune], ci, cj)
     // drawSpr(crateSprsA[inmune], ci, cj);
     // drawSpr(crateSpr, state[0], state[1]);
     // ctx.fillText((inmune+1).toString(), state[0]*TILE+OFFX, state[1]*TILE+OFFY);
@@ -400,7 +537,8 @@ function drawLevel (level) {
   pintar.drawSprite(player_sprite);
   //sortedCrates.reverse()
 
-  /*sortedCrates.forEach(crate => {
+  pintar._renderer.setShader(wobblyShader);
+  sortedCrates.forEach(crate => {
     if (crate.inHole.get()) return
     let state = crate.history.at(-1)
     let prevState = crate.history.at(-2)
@@ -409,11 +547,11 @@ function drawLevel (level) {
     let crate_forward = get_times_directions(crate.history.length - 2)[inmune] == 1
     let ci = lerp(prevState[0], state[0], crate_forward ? forwardsT : backwardsT)
     let cj = lerp(prevState[1], state[1], crate_forward ? forwardsT : backwardsT)
-    drawSpr(crateSprsB[inmune], ci, cj)
+    drawSpr(crate_sprites[inmune], ci, cj)
     // drawSpr(crateSpr, state[0], state[1]);
     // ctx.fillText((inmune+1).toString(), state[0]*TILE+OFFX, state[1]*TILE+OFFY);
     // result[state[1]][state[0]] += (inmune + 1).toString();
-  })*/
+  })
 
   /*drawEntranceGradient(level)
   drawExitGradient(level)*/
@@ -1055,7 +1193,10 @@ function movesBackToEntrance (level, pi, pj, cur_di, cur_dj) {
   return pi == si && pj == sj && cur_di == -level.enter[0] && cur_dj == -level.enter[1]
 }
 
-function draw () {
+function draw (timestamp) {
+  if (!last_t) last_t = timestamp
+  globalT += timestamp - last_t
+  last_t = timestamp
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
   // ctx.fillStyle = ALLOW_EDITOR ? COLORS.floorWin : COLORS.background // #75366D
   // ctx.fillStyle = COLORS.background // #75366D
