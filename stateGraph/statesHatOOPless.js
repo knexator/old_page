6b41888ed790555db10c5a7c8be989967e79c597
knexator <daniel.hurtadoramirez314@gmail.com>
2022-07-05 23:11:42 +0200

    standalone hats

--- stateGraph/statesHatOOPless.js
+++ stateGraph/statesHatOOPless.js
@@ -1,8 +1,9 @@
 //🐇,🕊️,🦇,🐸,🎩
 
+/*
 const initialResources = 'A,A,A,B,B,B'.split(',')
 // Experiment 1: last thing controls the operation
-// todo test: last thing is the argument to the operation
+// when last thing is the argument to the operation -> too much foresight
 const rules = [
   // applied in order, top to bottom
   ['A,A', 'A,A,A'], // A: triplicate second argument
@@ -16,19 +17,20 @@ const rules = [
   // C: delete second (in code)
   // D: swap next two (in code)
 ].map(x => [x[0].split(','), x[1].split(',')])
+// let targetGoal = 'D,C,B,A'.split(',')
 
 // apply some rule over the top N elements of hatContents
 // don't modify original array!
 function waveWand (hatContents) {
+  if (hatContents.length >= 3 && hatContents[0] == 'C') {
+    let newHatContents = hatContents.slice(1)
+    let e1 = newHatContents.shift()
+    let e2 = newHatContents.shift()
+    newHatContents.unshift(e1)
+    return newHatContents
+  }
   for (let k = 0; k < rules.length; k++) {
     const [condition, result] = rules[k]
-    if (hatContents.length >= 3 && hatContents[0] == 'C') {
-      let newHatContents = hatContents.slice(1)
-      let e1 = newHatContents.shift()
-      let e2 = newHatContents.shift()
-      newHatContents.unshift(e1)
-      return newHatContents
-    }
     if (hatContents.length >= 3 && hatContents[0] == 'D') {
       let newHatContents = hatContents.slice(1)
       let e1 = newHatContents.shift()
@@ -37,6 +39,53 @@ function waveWand (hatContents) {
       newHatContents.unshift(e2)
       return newHatContents
     }
+    if (checkCondition(hatContents, condition)) {
+      let newHatContents = hatContents.slice(condition.length)
+      newHatContents.unshift(...result)
+      return newHatContents
+    }
+  }
+  return false
+}
+*/
+
+// experiment 2: only 3 types
+const initialResources = 'A,A,A,B,B,B,C,C,C'.split(',')
+const rules = [
+  // applied in order, top to bottom
+  ['A,A', 'A,A,A'], // A: triplicate second argument
+  ['A,B', 'B,B,B'],
+  ['A,C', 'C,C,C']
+  // B: add missing piece (in code) - if 2 equal, delete both (or do something special?? triplicate?? idk)
+  // C: swap next two (in code)
+].map(x => [x[0].split(','), x[1].split(',')])
+const targetGoal = 'A,B,C,D'.split(',')
+
+// apply some rule over the top N elements of hatContents
+// don't modify original array!
+function waveWand (hatContents) {
+  if (hatContents.length >= 3 && hatContents[0] == 'B') {
+    let newHatContents = hatContents.slice(1)
+    let e1 = newHatContents.shift()
+    let e2 = newHatContents.shift()
+    if (e1 == e2) return false
+    ;['A', 'B', 'C'].forEach(letter => {
+      if (e1 !== letter && e2 !== letter) {
+        newHatContents.unshift(letter)
+      }
+    })
+    return newHatContents
+  }
+  if (hatContents.length >= 3 && hatContents[0] == 'C') {
+    let newHatContents = hatContents.slice(1)
+    let e1 = newHatContents.shift()
+    let e2 = newHatContents.shift()
+    newHatContents.unshift(e1)
+    newHatContents.unshift(e2)
+    return newHatContents
+  }
+  for (let k = 0; k < rules.length; k++) {
+    const [condition, result] = rules[k]
     if (checkCondition(hatContents, condition)) {
       let newHatContents = hatContents.slice(condition.length)
       newHatContents.unshift(...result)
@@ -119,8 +168,22 @@ State.id = function (state) {
   return [resourcesID, hatsID, shownID].join('\n')
 }
 
+State.textRepresentation = function (state) {
+  return State.id(state)
+  // let resourcesID = state.resources.join(',')
+  // let shownID = state.shown.join(',')
+  // return [resourcesID, shownID].join(';')
+}
+
 State.isWon = function (state) {
   return false
+  // return state.hats.some(
+  //   hat => JSON.stringify(hat.contents) == JSON.stringify(targetGoal)
+  // )
+}
+
+State.fakeIsWon = function (state, goalString) {
+  return state.hats.some(hat => JSON.stringify(hat.contents) == goalString)
 }
 
 State.copy = function (state) {
